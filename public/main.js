b12503cd93ec1b5137670c6125ef6892cc7eda90
BRUSCK <104568825+BernardoBrusck@users.noreply.github.com>
2023-11-28 23:46:08 -0300

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,27 @@
+function redirectTo(route) {
+    window.location.href = `http://localhost:3000${route}`;
+}
+
+document.querySelectorAll('.desenvolvimento').forEach(function (element) {
+    element.addEventListener('click', function () {
+        alert('Ainda em fase de desenvolvimento')
+    })
+})
+
+const usuarioNome = localStorage.getItem('usuarioNome');
+
+if (!usuarioNome) {
+    // Se não houver, redirecionar para a página de login
+    window.location.href = '/sign-in';
+} else {
+    // Aqui você pode realizar ações adicionais se o usuário estiver autenticado
+    console.log('Usuário autenticado:', usuarioNome);
+}
+
+function logout() {
+    // Remover 'usuarioNome' do localStorage
+    localStorage.removeItem('usuarioNome');
+
+    // Redirecionar para a página de login
+    window.location.href = '/sign-in';
+}
---
+++ public/sign.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const senhaInput = document.querySelector('.senha');
+    const confirmaSenhaInput = document.querySelector('.confirma_senha');
+    const verSenhaButton = document.querySelector('.ver_senha');
+
+    verSenhaButton.addEventListener('click', function () {
+        togglePasswordVisibility(senhaInput);
+        togglePasswordVisibility(confirmaSenhaInput);
+    });
+
+    // Função para alternar a visibilidade da senha
+    function togglePasswordVisibility(input) {
+        input.type = input.type === 'password' ? 'text' : 'password';
+    }
+});
